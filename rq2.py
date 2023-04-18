9abed03c1090803bfe2922426c75741467147ce5
Henrique Santos <hensantos@yahoo.com>
2023-04-18 13:26:56 -0400

    comments

--- rq2.py
+++ rq2.py
@@ -20,6 +20,9 @@ else:
 pred_file = "data/jsonl/" + file + ".jsonl"
 true_file = "data/jsonl/" + file + "-gt.jsonl"
 
+# Loads annotations for the parametrized dataset
+# Reads JSONL and builds the prediction matrix (pred_matrix)
+# Each column is a prompt, each row is an annotator
 with jsonlines.open(pred_file) as reader:
     for obj in reader:
         i = 0
@@ -27,6 +30,9 @@ with jsonlines.open(pred_file) as reader:
             pred_matrix[i].append(label["label"])
             i = i + 1
 
+# Loads ground-truth for the parametrized dataset
+# Reads JSONL and builds the modal ground-truth matrix (true_matrix)
+# Each column is a prompt, each row is an annotator
 with jsonlines.open(true_file) as reader:
     for obj in reader:
         i = 0
@@ -34,18 +40,23 @@ with jsonlines.open(true_file) as reader:
             true_matrix[i].append(label["label"])
             i = i + 1
 
+# Removes all missing values from the prediction matrix and...
+# ...respective values in the ground-truth matrix (cleaned_pred, cleaned_true)
 cleaned_true = [[],[],[],[],[],[]]
 cleaned_pred = [[],[],[],[],[],[]]
-
 for annotator in range(6):
     for i, x in enumerate(pred_matrix[annotator]):
         if str(x) != 'nan':
             cleaned_pred[annotator].append(x)
             cleaned_true[annotator].append(true_matrix[annotator][i])
 
+# Obtains z_value for 95% CI
 confidence = 0.95
 z_value = scipy.stats.norm.ppf((1 + confidence) / 2.0)
 
+# One annotator at a time, calculates accuracy of the prediction against the modal ground truth,...
+# ...calculates the confidence interval assuming Gaussian distribution,
+# ...and prints the output
 for annotator in range(6):
     accuracy = accuracy_score(cleaned_true[annotator], cleaned_pred[annotator])
     size = len(cleaned_pred[annotator])
