dae5e72ca400212425b8c71ddb0b3a3aea3a4d8a
Henrique Santos <hensantos@yahoo.com>
2023-04-18 13:13:14 -0400

    include comments

--- rq1.py
+++ rq1.py
@@ -16,6 +16,9 @@ else:
 
 file = "data/jsonl/" + file + ".jsonl"
 
+# Loads annotations for the parametrized dataset
+# Reads JSONL and build the labels_matrix
+# Each column is an annotator, each row is a prompt
 with jsonlines.open(file) as reader:
     for obj in reader:
         labels_prompt = []
@@ -23,18 +26,21 @@ with jsonlines.open(file) as reader:
             labels_prompt.append(label["label"])
         labels_matrix.append(labels_prompt)
 
+# Calculates prompt means and stdevs, and annotator means
+# ddof=1 for stdevs indicate Sample Standard Deviation instead of Population
 prompt_means = np.nanmean(labels_matrix, axis=1)
 prompt_stdevs = np.nanstd(labels_matrix, axis=1, ddof=1)
 annotator_means = np.nanmean(labels_matrix, axis=0)
 
-print(annotator_means)
-
+# Calculates equations (1), (2), (3), and (4) from the article
+# ddof=1 for stdevs indicate Sample Standard Deviation instead of Population
 level_noise = np.nanstd(annotator_means, ddof=1)
 pattern_noise_orig = np.nanstd(prompt_means, ddof=1)
 pattern_noise_mod = np.nanstd(prompt_stdevs, ddof=1)
 system_noise_orig = math.sqrt((level_noise ** 2) + (pattern_noise_orig ** 2))
 system_noise_mod = math.sqrt((level_noise ** 2) + (pattern_noise_mod ** 2))
 
+# Prepares output
 results = []
 results.append(["Level noise", level_noise])
 results.append(["Pattern noise (original)", pattern_noise_orig])
@@ -42,6 +48,7 @@ results.append(["Pattern noise (modified)", pattern_noise_mod])
 results.append(["System noise (original)", system_noise_orig])
 results.append(["System noise (modified)", system_noise_mod])
 
+# Prints output
 for result in results:
     name, value = result
     print ("{:>25}  {:<15}".format(name, value))
